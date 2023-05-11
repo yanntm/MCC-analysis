785bb186fd9616d0ce9d5a4d819849d27122ce20
Yann Thierry-Mieg <Yann.Thierry-Mieg@lip6.fr>
2023-05-11 13:49:11 +0200

    with GPT4, finish the code.
    this is pretty awesome honestly.

--- buildPages.py
+++ buildPages.py
@@ -2,11 +2,38 @@ import pandas as pd
 import glob
 import time
 
+def custom_html_table(df):
+    classes = []
+    for col in df.columns:
+        if col == df.columns[0]:
+            classes.append("tool")
+        elif col.endswith("error_total"):
+            classes.append("total errors")
+        elif col.endswith("_total"):
+            classes.append("total")
+        elif col.startswith("error"):
+            classes.append("details errors")
+        else:
+            classes.append("details")
+
+    table_html = '<table id="myTable" class="display">'
+    
+    header = '<thead><tr>'
+    for i, col in enumerate(df.columns):
+        header += f'<th class="{classes[i]}">{col}</th>'
+    header += '</tr></thead>'
+    table_html += header
+
+    table_html += df.to_html(header=False, index=False, classes="display", justify="left", escape=False, table_id=None)
+    table_html = table_html.replace('<table border="1" class="dataframe display">', '')
+    table_html = table_html.replace('</table>', '')
+    table_html += '</table>'
+    return table_html
 
 def generate_html(df, output_filename):
-    html_table = df.to_html(index=False, classes='display', justify='left', table_id="myTable")
+    html_table = custom_html_table(df)
 
-    html = """
+    html = f"""
     <!DOCTYPE html>
     <html>
     <head>
@@ -15,18 +42,39 @@ def generate_html(df, output_filename):
       <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.13.4/js/jquery.dataTables.js"></script>
      </head>
     <body>
-      {}
+      <label><input type="checkbox" id="detailsToggle" checked> Show details</label>
+      <label><input type="checkbox" id="errorsToggle" checked> Show errors</label>
+      {html_table}
       <script>
         $(document).ready(function() {{
-          $('#myTable').DataTable({{
+          let detailsVisible = true;
+          let errorsVisible = true;
+          let table = $('#myTable').DataTable({{
             "paging": false,
             "order": [[1, "asc"]]
           }});
+
+          function updateVisibility() {{
+            table.columns('.details:not(.errors)').visible(detailsVisible);
+            table.columns('.details.errors').visible(detailsVisible && errorsVisible);
+            table.columns('.total.errors').visible(errorsVisible);
+          }}
+
+          $('#detailsToggle').on('change', function() {{
+            detailsVisible = this.checked;
+            updateVisibility();
+          }});
+
+          $('#errorsToggle').on('change', function() {{
+            errorsVisible = this.checked;
+            updateVisibility();
+          }});
         }});
       </script>
+
     </body>
     </html>
-    """.format(html_table)
+    """
 
     with open(output_filename, 'w') as f:
         f.write(html)
@@ -43,12 +91,10 @@ for csv_file in csv_files:
     # Load the CSV file into a pandas dataframe
     df = pd.read_csv(csv_file)
 
-    # Sort the dataframe by the first column (change as needed)
-    df = df.sort_values(by=df.columns[0])
-
     # Generate the HTML table for the current CSV file
     output_filename = csv_file.replace('.csv', '.html')
     generate_html(df, output_filename)
 
     elapsed_time = time.time() - start_time  # Calculate the elapsed time for processing the current file
     print(f"Processed {csv_file} in {elapsed_time:.2f} seconds.")  # Print the elapsed time for processing the current file
+
