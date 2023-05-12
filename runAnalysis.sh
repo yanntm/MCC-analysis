de94465bff92ec1b3a6114b388f69de53550f2dd
Yann Thierry-Mieg <Yann.Thierry-Mieg@lip6.fr>
2023-05-12 15:30:47 +0200

    let's go ! 2017 to 2023 now available

--- runAnalysis.sh
+++ runAnalysis.sh
@@ -1,40 +1,54 @@
 #!/bin/bash
 
-# Create the "website" folder 
-mkdir -p website
+set -x
 
-# Download and unzip the results file
-if [ ! -f raw-result-analysis.csv.zip ] ; then
-	curl -O https://mcc.lip6.fr/2023/archives/raw-result-analysis.csv.zip
-fi
+download_data() {
+  local YEAR=$1
+  echo "Downloading data for year ${YEAR}..."
+  
+  if [ ! -f "raw-result-analysis.csv.zip" ] ; then
+    curl "https://mcc.lip6.fr/${YEAR}/archives/raw-result-analysis.csv.zip" -o "raw-result-analysis.csv.zip"
+  fi
 
-if [ ! -f raw-result-analysis.csv ] ; then
-	unzip raw-result-analysis.csv.zip
-	rm -rf __MACOSX
-fi
+  if [ ! -f "raw-result-analysis.csv" ] ; then
+    unzip "raw-result-analysis.csv.zip" 
+    rm -rf __MACOSX
+  fi
+}
 
-# Run the R script
-Rscript analyzeAnswers.R
-rm website/*.log
-
-
-# Convert TIFF images to PNG format
-cd website
-for tiff_image in *.tiff; do
+process_year() {
+  local year="$1"
+  echo "Crunching data for $year"
+  download_data "$year"
+  mkdir website
+  Rscript ../../analyzeAnswers.R 
+  
+  rm website/*.log
+  # Convert TIFF images to PNG format
+  cd website
+  for tiff_image in *.tiff; do
     png_image="${tiff_image%.*}.png"
     convert "$tiff_image" "$png_image"
     rm "$tiff_image"
+  done
+  cd ..
+  
+  python3 ../../buildPages.py 
+  mv *.html website/
+  python3 ../../buildFinalPages.py 
+}
+
+mkdir -p website
+cd website
+
+for year in {2017..2023}; do
+	mkdir $year
+	cd $year
+	cp -r ../../templates .
+  	process_year "$year"
+  	cd ..
 done
+
 cd ..
 
-# Run the Python script
-python3 buildPages.py
-mv *.html website/
-
-python3 buildFinalPages.py
-
-
-# Remove the downloaded zip file and raw-result-analysis.csv
-#rm raw-result-analysis.csv.zip
-#rm raw-result-analysis.csv
-
+echo "Finished processing all years."
