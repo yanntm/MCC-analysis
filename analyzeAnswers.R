d239a2c649c3728029729417283e35e1010b1f18
Yann Thierry-Mieg <yann.thierry-mieg@lip6.fr>
2023-05-13 14:00:24 +0200

    Debug and patch RBVT computation

--- analyzeAnswers.R
+++ analyzeAnswers.R
@@ -8,7 +8,7 @@ library(writexl)
 args <- commandArgs(trailingOnly = TRUE)
 
 # Set the current year, default to 2023 if not provided
-current_year <- if (length(args) >= 1) as.integer(args[1]) else 2017
+current_year <- if (length(args) >= 1) as.integer(args[1]) else 2023
 
 
 # Set default values for input file and output folder
@@ -51,24 +51,155 @@ ideal_tool_scores <- ideal_tool_scores[,-c(2)]
 scores <- rbind(scores, ideal_tool_scores)
 
 
-calculate_bvt_scores <- function(df, examinations) {
-  df_filtered <- df %>%
-    filter(Examination %in% examinations) %>%
-    group_by(Input, Examination) %>%
-    summarize(score = max(str_count(mask, "[T]")))
-  
-  bvt_scores <- df_filtered %>%
-    group_by(Examination) %>%
-    summarize(score = sum(score))
-  
-  return(bvt_scores)
-}
-bvt_scores <- calculate_bvt_scores(df, unique(df$Examination))
+# Split mask column into individual characters
+max_mask_width <- max(nchar(gsub("[\\s?-]+", "", df$mask)))  # Get the widest mask
+df_mask_split <- strsplit(gsub("[\\s?-]+", "", df$mask), "")  # Split mask into individual characters, replacing "?" and "-" with an empty string
+df_mask_split <- lapply(df_mask_split, function(x) c(x, rep(NA, max_mask_width - length(x))))  # Pad with NA as needed
+df_mask_split <- do.call(rbind, df_mask_split)  # Convert list to dataframe
+colnames(df_mask_split) <- paste0("res_", seq_len(max_mask_width))
 
-bvt_scores$row <- paste0("RBVT-", current_year)
-colnames(bvt_scores) <- c("Examination", "score", "tool")
-bvt_scores$errors <- 0
-scores <- rbind(scores, bvt_scores)
+# Combine the original and split mask dataframes
+df_new <- cbind(df[, 1:3], df_mask_split)
+colnames(df_new)
+
+# Load the required libraries
+library(VennDiagram)
+
+# Reshape the data frame into long format
+df_long <- pivot_longer(df_new, cols = starts_with("Input"), names_to = "Input_index", values_to = "Input") %>%
+  pivot_longer(cols = starts_with("res"), names_to = "result_index", values_to = "result")
+
+df_long <- unite(df_long, "Input", c("Input", "result_index"), sep = "_")
+df_long <- df_long[,c(1,2,4,5)]
+
+# Function to generate Venn diagrams for a given category
+generate_venn_diagrams <- function(df_category, category_name) {
+  # Split the dataframe by the tool column
+  tools_list <- split(df_category, df_category$tool)
+  
+  # Create a list of non-NA results for each tool
+  tool_results <- lapply(tools_list, function(tool_df) {
+    tool_df %>%
+      filter(!is.na(result) & result != "X") %>%
+      select(Input, result) %>%
+      unique()
+  })
+  
+  # Compute the BVT results as the union of other tools' results
+  bvt_results <- unique(do.call(rbind, tool_results))
+  
+  # Add a name for the BVT results and append it to the tool_results list
+  names(bvt_results) <- paste0("RBVT-", current_year)
+  tool_results <- c(tool_results, list(bvt_results))
+  
+  # Calculate set sizes for each index
+  sets <- lapply(tool_results, function(x) x$Input)
+  set_sizes <- sapply(sets, function(x) length(x))
+  
+  # Filter out tools with a set size of 0 (i.e., the tool did not participate in the category)
+  non_empty_indices <- which(set_sizes != 0)
+  tool_results <- tool_results[non_empty_indices]
+  set_sizes <- set_sizes[non_empty_indices]
+  
+  # Get order of set sizes
+  set_order <- order(set_sizes, decreasing = TRUE)
+  
+  # Use order to create sorted list of tools
+  sorted_tools <- tool_results[set_order]
+  
+  # Create Venn diagrams for each tool and its two next ones
+  for (i in 1:(length(sorted_tools) - 2)) {
+    # Get current tool and its two next ones
+    current_tool <- sorted_tools[[i]]$Input
+    next_tool_1 <- sorted_tools[[i + 1]]$Input
+    next_tool_2 <- sorted_tools[[i + 2]]$Input
+    
+    # Create Venn diagram with these tools
+    tool_names <- names(sorted_tools)
+    venn.diagram(
+      x = list(current_tool = sorted_tools[[i]]$Input,
+               next_tool_1 = sorted_tools[[i + 1]]$Input,
+               next_tool_2 = sorted_tools[[i + 2]]$Input),
+      category.names = tool_names[c(i, i + 1, i + 2)],
+      filename = paste0(output_folder, "/", category_name, "_", i, "_venn.tiff"),
+      main = paste(category_name, ":", tool_names[i], "(", set_sizes[set_order[i]], ") vs",
+                   tool_names[i + 1], "(", set_sizes[set_order[i + 1]], ") vs",
+                   tool_names[i + 2], "(", set_sizes[set_order[i + 2]], ")"),
+      
+      output = TRUE
+    )
+  }
+  
+}
+
+
+# Define categories
+categories <- list(
+  state_space = c("StateSpace"),
+  global_properties = c("Liveness", "QuasiLiveness", "StableMarking", "ReachabilityDeadlock", "OneSafe"),
+  reachability = c("ReachabilityCardinality", "ReachabilityFireability"),
+  ctl = c("CTLCardinality", "CTLFireability"),
+  ltl = c("LTLCardinality", "LTLFireability"),
+  upper_bounds = c("UpperBounds")
+)
+
+# Iterate through each category to create Venn diagrams
+for (category_name in names(categories)) {
+  examinations <- categories[[category_name]]
+  df_category <- subset(df_long, Examination %in% examinations)
+  df_category <- unite(df_category, "Input", c("Examination", "Input"), sep = "_")
+  
+  # Call the generate_venn_diagrams function for the current category
+  generate_venn_diagrams(df_category, category_name)
+}
+
+
+compute_bvt_score <- function(df, examination) {
+  df_examination <- df %>%
+    filter(Examination == examination) %>%
+    unite(Input, c("Examination", "Input"), sep = "_")
+  
+  tools_list <- split(df_examination, df_examination$tool)
+  
+  tool_results <- lapply(tools_list, function(tool_df) {
+    tool_df %>%
+      filter(!is.na(result)& result != "X") %>%
+      select(Input, result) %>%
+      unique()
+  })
+  
+  bvt_results <- unique(do.call(rbind, tool_results))
+  
+  # Debug step: Compare the answer sets of BVT-2023 and RBVT
+  if ("BVT-2023" %in% names(tools_list)) {
+    bvt_2023_results <- tools_list[["BVT-2023"]] %>%
+      filter(!is.na(result)) %>%  # Filter out NA elements from BVT-2023
+      select(Input, result) %>%
+      unique()
+    
+    rbvt_only <- setdiff(bvt_results, bvt_2023_results)
+    bvt_2023_only <- setdiff(bvt_2023_results, bvt_results)
+    
+    if (nrow(rbvt_only) > 0 || nrow(bvt_2023_only) > 0) {
+      cat("\nExamination:", examination)
+      cat("\nRBVT-2023 only results:\n")
+      print(rbvt_only)
+      cat("\nBVT-2023 only results:\n")
+      print(bvt_2023_only)
+    }
+  }
+  
+  return(nrow(bvt_results))
+}
+
+unique_examinations <- unique(df_long$Examination)
+
+for (examination in unique_examinations) {
+  bvt_score <- compute_bvt_score(df_long, examination)
+  scores <- rbind(scores, data.frame(Examination = examination, score = bvt_score, tool = paste0("RBVT-", current_year), errors = 0))
+}
+
+
 
 # Pivot the table wider, creating separate columns for scores and errors
 scores_wide <- scores %>%
@@ -144,102 +275,3 @@ write.csv(df_ltl, "ltl.csv", row.names = FALSE)
 df_upper_bounds <- df_reordered[, c("tool", "score_UpperBounds", "errors_UpperBounds")]
 df_upper_bounds <- df_upper_bounds[rowSums(df_upper_bounds[, "score_UpperBounds"]) != 0,]
 write.csv(df_upper_bounds, "upper_bounds.csv", row.names = FALSE)
-
-# Split mask column into individual characters
-max_mask_width <- max(nchar(gsub("[\\s?-]+", "", df$mask)))  # Get the widest mask
-df_mask_split <- strsplit(gsub("[\\s?-]+", "", df$mask), "")  # Split mask into individual characters, replacing "?" and "-" with an empty string
-df_mask_split <- lapply(df_mask_split, function(x) c(x, rep(NA, max_mask_width - length(x))))  # Pad with NA as needed
-df_mask_split <- do.call(rbind, df_mask_split)  # Convert list to dataframe
-colnames(df_mask_split) <- paste0("res_", seq_len(max_mask_width))
-
-# Combine the original and split mask dataframes
-df_new <- cbind(df[, 1:3], df_mask_split)
-colnames(df_new)
-
-# Load the required libraries
-library(VennDiagram)
-
-# Reshape the data frame into long format
-df_long <- pivot_longer(df_new, cols = starts_with("Input"), names_to = "Input_index", values_to = "Input") %>%
-  pivot_longer(cols = starts_with("res"), names_to = "result_index", values_to = "result")
-
-df_long <- unite(df_long, "Input", c("Input", "result_index"), sep = "_")
-df_long <- df_long[,c(1,2,4,5)]
-
-# Function to generate Venn diagrams for a given category
-generate_venn_diagrams <- function(df_category, category_name) {
-  # Split the dataframe by the tool column
-  tools_list <- split(df_category, df_category$tool)
-
-  # Create a list of non-NA results for each tool
-  tool_results <- lapply(tools_list, function(tool_df) {
-    tool_df %>%
-      filter(!is.na(result)) %>%
-      select(Input, result) %>%
-      unique()
-  })
-  
-  # Compute the BVT results as the union of other tools' results
-  bvt_results <- unique(do.call(rbind, tool_results))
-
-  # Add a name for the BVT results and append it to the tool_results list
-  names(bvt_results) <- paste0("RBVT-", current_year)
-  tool_results <- c(tool_results, list(bvt_results))
-
-  # Calculate set sizes for each index
-  sets <- lapply(tool_results, function(x) x$Input)
-  set_sizes <- sapply(sets, function(x) length(x))
-
-  # Filter out tools with a set size of 0 (i.e., the tool did not participate in the category)
-  non_empty_indices <- which(set_sizes != 0)
-  tool_results <- tool_results[non_empty_indices]
-  set_sizes <- set_sizes[non_empty_indices]
-
-  # Get order of set sizes
-  set_order <- order(set_sizes, decreasing = TRUE)
-
-  # Use order to create sorted list of tools
-  sorted_tools <- tool_results[set_order]
-
-  # Create Venn diagrams for each tool and its two next ones
-  for (i in 1:(length(sorted_tools) - 2)) {
-    # Get current tool and its two next ones
-    current_tool <- sorted_tools[[i]]$Input
-    next_tool_1 <- sorted_tools[[i + 1]]$Input
-    next_tool_2 <- sorted_tools[[i + 2]]$Input
-
-    # Create Venn diagram with these tools
-    tool_names <- names(sorted_tools)
-    venn.diagram(
-      x = list(current_tool, next_tool_1, next_tool_2),
-      category.names = tool_names[c(i, i + 1, i + 2)],
-	  filename = paste0(output_folder, "/", category_name, "_", i, "_venn.tiff"),
-	  main = paste(category_name, ":", tool_names[i], "(", set_sizes[set_order[i]], ") vs",
-               tool_names[i + 1], "(", set_sizes[set_order[i + 1]], ") vs",
-               tool_names[i + 2], "(", set_sizes[set_order[i + 2]], ")"),
-
-      output = TRUE
-    )
-  }
-}
-
-
-# Define categories
-categories <- list(
-  state_space = c("StateSpace"),
-  global_properties = c("Liveness", "QuasiLiveness", "StableMarking", "ReachabilityDeadlock", "OneSafe"),
-  reachability = c("ReachabilityCardinality", "ReachabilityFireability"),
-  ctl = c("CTLCardinality", "CTLFireability"),
-  ltl = c("LTLCardinality", "LTLFireability"),
-  upper_bounds = c("UpperBounds")
-)
-
-# Iterate through each category to create Venn diagrams
-for (category_name in names(categories)) {
-  examinations <- categories[[category_name]]
-  df_category <- subset(df_long, Examination %in% examinations)
-  df_category <- unite(df_category, "Input", c("Examination", "Input"), sep = "_")
-  
-  # Call the generate_venn_diagrams function for the current category
-  generate_venn_diagrams(df_category, category_name)
-}
