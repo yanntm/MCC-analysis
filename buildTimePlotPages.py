3db154aa724ccf05011ee9b0512411a9f4513071
Yann Thierry-Mieg <Yann.Thierry-Mieg@lip6.fr>
2023-05-17 15:36:18 +0200

    pluriannual plots have learnt to dal with categories

--- buildTimePlotPages.py
+++ buildTimePlotPages.py
@@ -22,30 +22,41 @@ with open('timeplots.html', 'w') as f:
     f.write(html_output)
 
 
+# Define categories and examinations
+categories = {
+    "state_space": ["StateSpace"],
+    "global_properties": ["Liveness", "QuasiLiveness", "StableMarking", "ReachabilityDeadlock", "OneSafe"],
+    "reachability": ["ReachabilityCardinality", "ReachabilityFireability"],
+    "ctl": ["CTLCardinality", "CTLFireability"],
+    "ltl": ["LTLCardinality", "LTLFireability"],
+    "upper_bounds": ["UpperBounds"]
+}
+
 # Get the annual_plot.html template
 template = env.get_template('annual_plot.html')
- 
+
 # Create a list to hold links to the individual plot pages
 plot_links = []
 
-# Loop over all CSV files in the csv/ directory
-for csv_file in glob.glob('csv/*.csv'):
-  # Compute the plot_id, norm_score_col, and score_col
-  base_name = os.path.basename(csv_file)
-  score_col = base_name.replace('_time.csv', '')
-  norm_score_col = 'norm_' + score_col
-  plot_id = 'plot_' + score_col.replace(' ', '_')
+# Loop over all categories
+for category, examinations in categories.items():
+    # List of CSV files for the category and its examinations
+    csv_files = [f'csv/score_{category}_time.csv']
+    csv_files += [f'csv/score_{examination}_time.csv' for examination in examinations]
+    
+    # Compute the plot_id
+    plot_id = 'plot_' + category
 
-  # Render the template with the CSV data
-  html = template.render(plot_id=plot_id, csv_file=csv_file, norm_score_col=norm_score_col, score_col=score_col)
+    # Render the template with the CSV data
+    html = template.render(plot_id=plot_id, csv_files=csv_files)
 
-  # Write the rendered HTML to a file
-  html_file = './' + score_col + '_annual.html'
-  with open(html_file, 'w') as f:
-    f.write(html)
+    # Write the rendered HTML to a file
+    html_file = './' + category + '_annual.html'
+    with open(html_file, 'w') as f:
+        f.write(html)
 
-  # Add a link to this plot page to the list of plot links
-  plot_links.append((score_col, html_file))
+    # Add a link to this plot page to the list of plot links
+    plot_links.append((category, html_file))
 
 # Get the pluriannual_plots.html template
 template = env.get_template('pluriannual_plots.html')
