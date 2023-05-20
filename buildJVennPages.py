22dab097b78eb2febbafa3c253efa956d695b2f7
Yann Thierry-Mieg <Yann.Thierry-Mieg@lip6.fr>
2023-05-20 16:51:36 +0200

    adding filters for ModelType

--- buildJVennPages.py
+++ buildJVennPages.py
@@ -15,10 +15,41 @@ for file_name in os.listdir(json_dir):
         tool_name = os.path.splitext(file_name)[0]  # Get the tool name (file name without extension)
         with open(os.path.join(json_dir, file_name)) as f:
             data = json.load(f)
-            tool_index_dict[tool_name] = data['answers']  # Store the "answers" indices in the dictionary
+            # Convert the "answers" indices into strings
+            tool_index_dict[tool_name] = [str(index) for index in data['answers']]
 
+# Render the tool_index_dict into a JSON string
+json_str = json.dumps(tool_index_dict)
+
+# Write the JSON string to a file
+with open('tool_index_dict.json', 'w') as f:
+    f.write(json_str)
+    
+    
 # Create a sorted list of tools by the number of elements in their set
 sorted_tools = sorted(tool_index_dict.keys(), key=lambda x: len(tool_index_dict[x]), reverse=True)
+
+# Read the resolution file into a pandas DataFrame
+df_resolution = pd.read_csv('resolution.csv')
+
+# Create a dictionary to store model types and their corresponding indexes
+model_type_dict = {}
+
+# Load the data from the CSV file
+df = pd.read_csv('resolution.csv')
+
+# Get the unique model types
+unique_model_types = df['ModelType'].unique().tolist()
+
+# Fill the model_type_dict
+for model_type in unique_model_types:
+    index_set = set(df[df['ModelType'] == model_type]['Index'].astype(str))
+    model_type_dict[model_type] = index_set
+
+# Write model type dictionaries to JSON files
+for model_type, index_set in model_type_dict.items():
+    with open(f'{model_type}.json', 'w') as f:
+        json.dump(sorted(list(index_set)), f)  # convert set to list before dumping as JSON
 
 # Set up Jinja2 template environment
 env = Environment(loader=FileSystemLoader("templates"))
@@ -27,7 +58,7 @@ env = Environment(loader=FileSystemLoader("templates"))
 template = env.get_template('jvenn.html')
 
 # Render template with the tool_index_dict
-output = template.render(tool_index_dict=tool_index_dict, sorted_tools=sorted_tools)
+output = template.render(tool_index_dict=tool_index_dict, sorted_tools=sorted_tools, model_types=unique_model_types)
 
 # Write the output to a HTML file
 with open('venn_dynamic.html', 'w') as f:
