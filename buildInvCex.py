8088cc99c97521fb7939bc5f5297689bd1d0e9cd
Yann Thierry-Mieg <Yann.Thierry-Mieg@lip6.fr>
2023-06-30 19:50:16 +0100

    new plots on INV/CEX

--- buildInvCex.py
+++ buildInvCex.py
@@ -2,24 +2,22 @@ from jinja2 import Environment, FileSystemLoader
 import pandas as pd
 import json
 
-# Load the data
-df = pd.read_csv("invcex/formulas.csv")
-
-# Convert the data to a JSON-like format
-data = []
-for formula_type in df['FormulaType'].unique():
-    df_subset = df[df['FormulaType'] == formula_type]
-    data.append({
-        'x': df_subset['Year'].tolist(),
-        'y': df_subset['Proportion'].tolist(),
-        'name': formula_type,
-        'type': 'bar'
-    })
-
 # Configure Jinja2 environment
 env = Environment(loader=FileSystemLoader('./templates'))
 
 # Create the index.html file
 index_template = env.get_template("invcex.html")
 with open("invcex/invcex.html", "w") as index_file:
-    index_file.write(index_template.render(data=json.dumps(data)))
+    index_file.write(index_template.render())
+
+
+# Create the toolinvcex.html file
+template = env.get_template("toolinvcex.html")
+with open("invcex/toolinvcex.html", "w") as out_file:
+    out_file.write(template.render())
+    
+
+# Create the toolinvcex.html file
+template = env.get_template("toolinvcexhard.html")
+with open("invcex/toolinvcexhard.html", "w") as out_file:
+    out_file.write(template.render())
